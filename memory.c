96f55145752ba7ca8edc6e54a30f2dfc08df0f74
Fatih Karahan <fkyroath@gmail.com>
2019-12-21 16:21:30 +0300

    Fourth Lesson

--- memory.c
+++ memory.c
@@ -2,6 +2,8 @@
 
 #include "common.h"
 #include "memory.h"
+#include "object.h"
+#include "vm.h"
 
 void* reallocate(void* previous, size_t oldSize, size_t newSize) {
     if (newSize == 0) {
@@ -10,4 +12,24 @@ void* reallocate(void* previous, size_t oldSize, size_t newSize) {
     }
 
     return realloc(previous, newSize);
+}
+
+static void freeObject(Obj* object) {
+    switch (object->type) {
+        case OBJ_STRING: {
+            ObjString* string = (ObjString*)object;
+            FREE_ARRAY(char, string->chars, string->length + 1);
+            FREE(ObjString, string);
+            break;
+        }
+    }
+}
+
+void freeObjects() {
+    Obj* object = vm.objects;
+    while (object != NULL) {
+        Obj* next = object->next;
+        freeObject(object);
+        object = next;
+    }
 }
